15929ec821b781f7f2aae1e3b6a0f293439e4e3b
Fine Wilms <fine.wilms@gmail.com>
2016-07-06 14:37:24 +0200

    modified sflux to also remove savuv and savuvt

--- sflux.f90
+++ sflux.f90
@@ -36,7 +36,6 @@ use nsibd_m                        ! Land-surface arrays
 use pbl_m                          ! Boundary layer arrays
 use permsurf_m                     ! Fixed surface arrays
 use river                          ! River routing
-use savuvt_m                       ! Saved dynamic arrays
 use sigs_m                         ! Atmosphere sigma levels
 use soil_m                         ! Soil and surface data
 use soilsnow_m                     ! Soil, snow and surface data
